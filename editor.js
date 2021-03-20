7120eaa4ef6abb0fbdaf107940cbf5ad63044736
Spheali <byronli8565@gmail.com>
2021-03-20 12:46:06 -0500

    grammar module

--- editor.js
+++ editor.js
@@ -1,14 +1,55 @@
+window.BeyondGrammar.initBeyondGrammar();
 
-var options = {
-    modules: {
-        toolbar: [
-            [{ header: [1, 2, false] }],
-            ['bold', 'italic', 'underline'],
-            ['image', 'code-block']
-        ]
+var quill = new Quill('#editor-container', {
+  theme: 'snow',
+  modules: {
+    toolbar: {
+      container: [
+        [{ header: ['1', '2', '3', false] }],
+        ['bold', 'italic', 'underline', 'link'],
+        [{ list: 'ordered' }, { list: 'bullet' }],
+        ['clean'],
+        [{ beyondgrammar: ['en-US', 'en-GB', false] }]
+      ],
+      handlers: {
+        beyondgrammar: window.BeyondGrammar.getToolbarHandler(quill)
+      }
     },
-    placeholder: 'Your essay goes here!',
-    theme: 'snow'  // or 'bubble
-        };
+    beyondgrammar: {
+    	 service: {
 
-var quill = new Quill('#editor-container', options);
+      //You should signup for getting this key
+      apiKey: "FD111F92-42A2-4935-A433-4D34716575A0",
+
+      //[optional] You can specify it for permanent access to your settings and dictionaries
+      //userId: "<YOUR_USER_ID>",
+
+      //[optional] path to js file with BeyondGrammar Core
+      //sourcePath : "", 
+
+      //[optional] path to service which provides grammar checking
+      // url shouldn's contains / in the end
+      //serviceUrl : "https://rtg.prowritingaid.com/api/v1", 
+    },
+    grammar: {
+      //[optional] Default language [en-US, en-GB],
+      languageIsoCode: "en-US",
+
+      //[optional] checking Style. By default is "true"
+      checkStyle: true,
+
+      //[optional] checking Spelling. By default is "true"
+      checkSpelling: true,
+
+      //[optional] checking Grammar. By default is "true"
+      checkGrammar: true,
+
+      //[optional] Show thesaurus information by double click, by default true
+      showThesaurusByDoubleClick: true,
+
+      //[optional] Showing context thesaurus, works only if showThesaurusByDoubleClick = true, by default false
+      showContextThesaurus: false,
+    }
+    }
+  }
+});
