423894cc0a611cba02852eeed7bfdc2079f1fad8
Spheali <byronli8565@gmail.com>
2021-03-20 10:48:03 -0500

    uninstalled npm lol

--- editor.js
+++ editor.js
@@ -1,6 +1,14 @@
 
+var options = {
+    modules: {
+        toolbar: [
+            [{ header: [1, 2, false] }],
+            ['bold', 'italic', 'underline'],
+            ['image', 'code-block']
+        ]
+    },
+    placeholder: 'Your essay goes here!',
+    theme: 'snow'  // or 'bubble
+        };
 
-
-var quill = new Quill('#editor', {
-    theme: 'snow'
-});
+var quill = new Quill('#editor-container', options);
