50e98fb5aec4dfe43a1351cad87e24f5783d9792
Neil Song <song.neil.song@gmail.com>
2021-03-20 14:56:35 -0500

    create checkpoint work

--- frontend/react-packhacks/src/index.js
+++ frontend/react-packhacks/src/index.js
@@ -1,13 +1,7 @@
 import React, {Component, useState} from 'react'
 import ReactDOM from 'react-dom'
 
-import ReactQuill, {Quill} from 'react-quill';
-import {DragAndDropModule} from 'quill-drag-and-drop-module'
-import 'react-quill/dist/quill.snow.css';
-Quill.register('modules/DragAndDropModule', DragAndDropModule);
-
 function MyComponent() {
-  const [value, setValue] = useState('');
 
   return (
     <ReactQuill theme="snow" value={value} onChange={setValue}/>
